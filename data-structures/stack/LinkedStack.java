ea229f9e25478870a08d0004dd30f9848f567ae5
shipitsteven <stevenwangbe@gmail.com>
2021-01-15 12:21:18 -0800

    ignored blueJ project files, changed placeholder file to .java

---
+++ data-structures/stack/LinkedStack.java
@@ -0,0 +1,12 @@
+public class LinkedStack
+{
+
+    private class ListNode<T> {        
+        T data;
+        ListNode next;
+        
+        public ListNode(T value){
+            data = value;
+        }
+    }
+}
