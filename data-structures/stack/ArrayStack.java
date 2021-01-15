059ef308565d929503324bb1c04f25001d616ec9
shipitsteven <stevenwangbe@gmail.com>
2021-01-15 15:49:50 -0800

    finished LinkedStack, started ArrayStack

---
+++ data-structures/stack/ArrayStack.java
@@ -0,0 +1,17 @@
+/**
+ * Stack implemented with an array
+ */
+public class ArrayStack<T>
+{
+    private T[] array;
+    private int size;
+    
+    public ArrayStack(){
+        array = (T[]) new Object[1];
+    }
+    
+    public void push(T value){
+    }
+    
+    //public T pop(){}
+}
--- data-structures/stack/LinkedStack.java
+++ data-structures/stack/LinkedStack.java
@@ -1,11 +1,34 @@
-public class LinkedStack
-{
 
-    private class ListNode<T> {        
+
+
+/**
+ * Stack implemented with LinkedList and Generics
+ */
+public class LinkedStack<T>
+{
+    private Node first;
+    
+    public void push(T value){
+        Node oldFirst = first;
+        first = new Node(value);
+        first.next = oldFirst;
+    }
+    
+    public T pop(){
+        if(first != null){
+            T value = first.data;
+            first = first.next;
+            return value;
+        } else {
+            throw new java.util.EmptyStackException();
+        }
+    }
+    
+    private class Node {        
         T data;
-        ListNode next;
+        Node next;
         
-        public ListNode(T value){
+        public Node(T value){
             data = value;
         }
     }
