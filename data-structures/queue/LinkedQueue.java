5202b3dd7edea44a8ee71c2d15077b6353dba22a
swang93 <swang93@uw.edu>
2021-01-16 11:43:44 -0800

    finished LinkedQueue

--- data-structures/queue/LinkedQueue.java
+++ data-structures/queue/LinkedQueue.java
@@ -1,23 +1,52 @@
+/**
+ * Queue implemented with LinkedList and Generics
+ */
 public class LinkedQueue<T> {
-    ListNode front;
-    ListNode back;
+    Node first;
+    Node last;
 
-    public void enqueue(T value){
-        ListNode current = front;
-        while(current != null){
-            current = current.next;
-        }
-        // is anything pointing at current?
-        current = new ListNode(value);
+    public boolean isEmpty() {
+        return first == null;
     }
 
-    private class ListNode {
-        T data;
-        ListNode next;
+    public void enqueue(T value) {
+        Node oldLast = last;
+        last = new Node(value);
+        if (isEmpty()) {
+            first = last;
+        } else {
+            oldLast.next = last;
+        }
+    }
 
-        public ListNode(T value){
+    public T dequeue() {
+        if (isEmpty()) {
+            throw new java.util.NoSuchElementException();
+        } else {
+            T value = first.data;
+            first = first.next;
+            if (isEmpty()) {
+                last = null;
+            }
+            return value;
+        }
+    }
+
+    private class Node {
+        T data;
+        Node next;
+
+        Node(T value) {
             data = value;
         }
     }
 
+    // public static void main(String[] args) {
+    // LinkedQueue<String> test = new LinkedQueue<>();
+    // test.enqueue("first");
+    // test.enqueue("second");
+    // System.out.println(test.dequeue()); // first
+    // System.out.println(test.dequeue()); // second
+    // System.out.println(test.dequeue()); // NoSuchElementException
+    // }
 }
--- data-structures/stack/ArrayStack.java
+++ data-structures/stack/ArrayStack.java
@@ -1,46 +1,45 @@
 /**
  * Stack implemented with an array
  */
-public class ArrayStack<T>
-{
-    private T[] array;
-    private int N;
-    
-    public ArrayStack(){
-        array = (T[]) new Object[1];
-    }
-    
-    public void push(T value){
-      if(N == array.length){
+public class ArrayStack<T> {
+   private T[] array;
+   private int N;
+
+   public ArrayStack() {
+      array = (T[]) new Object[1];
+   }
+
+   public void push(T value) {
+      if (N == array.length) {
          resize(2 * array.length + 1);
       }
       array[N++] = value;
-    }
-    
-    public T pop(){
-      if(N > 0){
+   }
+
+   public T pop() {
+      if (N > 0) {
          T data = array[--N];
          array[N] = null;
-         if(N > 0 && N == array.length / 4){
-            resize(array.length /2);
+         if (N > 0 && N == array.length / 4) {
+            resize(array.length / 2);
          }
          return data;
       } else {
          throw new java.util.EmptyStackException();
       }
-    }
-    
-    private void resize(int capacity){
+   }
+
+   private void resize(int capacity) {
       T[] copy = java.util.Arrays.copyOf(array, capacity);
       array = copy;
-    }
-    
-//     public static void main(String[] args){
-//       ArrayStack<String> test = new ArrayStack<>();
-//       test.push("first");
-//       test.push("second");
-//       System.out.println(test.pop()); // second
-//       System.out.println(test.pop()); // first
-//       System.out.println(test.pop()); // EmptyStackException     
-//     }
+   }
+
+   // public static void main(String[] args){
+   // ArrayStack<String> test = new ArrayStack<>();
+   // test.push("first");
+   // test.push("second");
+   // System.out.println(test.pop()); // second
+   // System.out.println(test.pop()); // first
+   // System.out.println(test.pop()); // EmptyStackException
+   // }
 }
