a301a456db01f9932c13da84d386883e39c3d13a
swang93 <swang93@uw.edu>
2021-01-16 22:21:08 -0800

    finished RandomQueue and Permutation

--- coursera-projects/queues/Permutation.java
+++ coursera-projects/queues/Permutation.java
@@ -4,8 +4,21 @@
  *  Description:
  **************************************************************************** */
 
+import edu.princeton.cs.algs4.StdIn;
+
+import java.util.Iterator;
+
 public class Permutation {
     public static void main(String[] args) {
-        
+        RandomizedQueue<String> rque = new RandomizedQueue<>();
+        int num = Integer.parseInt(args[0]);
+        String str;
+        while (!StdIn.isEmpty()) {
+            str = StdIn.readString();
+            rque.enqueue(str);
+        }
+        Iterator<String> iterator = rque.iterator();
+        for (int i = 0; i < num; i++)
+            System.out.println(iterator.next());
     }
 }
--- coursera-projects/queues/RandomizedQueue.java
+++ coursera-projects/queues/RandomizedQueue.java
@@ -4,45 +4,101 @@
  *  Description:
  **************************************************************************** */
 
-import java.util.Iterator;
+import edu.princeton.cs.algs4.StdRandom;
+
+import java.util.NoSuchElementException;
 
 public class RandomizedQueue<Item> implements Iterable<Item> {
 
+    private Item[] r;
+    private int size;
+
     // construct an empty randomized queue
-    public RandomizedQueue(){}
+    public RandomizedQueue() {
+        r = (Item[]) new Object[2];
+        size = 0;
+    }
 
     // is the randomized queue empty?
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
 
     // return the number of items on the randomized queue
     public int size() {
-        return 0;
+        return size;
     }
 
     // add the item
     public void enqueue(Item item) {
+        if (item == null) throw new IllegalArgumentException();
+        if (size == r.length) resize(r.length * 2 + 1);
+        r[size++] = item;
     }
 
     // remove and return a random item
     public Item dequeue() {
-        return null;
+        int randomNumber = StdRandom.uniform(size);
+        Item chosen = r[randomNumber];
+        r[randomNumber] = r[size - 1];
+        r[--size] = null;
+        if (!isEmpty() && size < r.length / 4) resize(r.length / 2);
+        return chosen;
     }
 
     // return a random item (but do not remove it)
     public Item sample() {
-        return null;
+        if (isEmpty()) throw new NoSuchElementException();
+        int randomNumber = StdRandom.uniform(size);
+        return r[randomNumber];
     }
 
     // return an independent iterator over items in random order
     public Iterator<Item> iterator() {
-        return null;
+        return new Iterator<Item>(r);
     }
+
+    private class Iterator<Item> implements java.util.Iterator<Item> {
+
+        Item[] itArray;
+        int current;
+
+        public Iterator(Item[] arr) {
+            itArray = (Item[]) new Object[arr.length];
+            System.arraycopy(arr,0, itArray, 0, arr.length);
+            StdRandom.shuffle(itArray);
+        }
+
+        public boolean hasNext() {
+            return current != itArray.length;
+        }
+
+        public Item next() {
+            if (!hasNext()) throw new NoSuchElementException();
+            return itArray[current++];
+        }
+
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
+    }
+
+    private void resize(int capacity) {
+        Item[] copy = (Item[]) new Object[r.length * 2 + 1];
+        System.arraycopy(r, 0, copy, 0, r.length);
+        r = copy;
+    }
+
 
     // unit testing (required)
     public static void main(String[] args) {
-
+        RandomizedQueue<Integer> rque = new RandomizedQueue<>();
+        for (int i = 0; i < 30; i++) {
+            rque.enqueue(i);
+        }
+        for (int i = 0; i < 30; i++) {
+            System.out.print(rque.dequeue() + ", ");
+        }
     }
 
 }
